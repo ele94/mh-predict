924bf02f4559871b6b8b48e1d26b5b97c18231ea
ele94 <lena.ageitos@gmail.com>
2021-03-26 17:10:00 +0100

    More tests

--- experiment_settings.py
+++ experiment_settings.py
@@ -19,7 +19,7 @@ def get_experiment_settings():
     new_params["weights_window_size"] = [100]
 
     new_params["feats"] = ["text"]
-    new_params["text_features"] = ["all"]
+    new_params["text_features"] = ["all", "select"]
     new_params["prons"] = [True]
     new_params["nssi"] = [True]
     new_params["tfidf_type"] = ["positives"]
