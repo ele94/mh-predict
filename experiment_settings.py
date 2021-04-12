4fa94aefc8a4fc5f5c0db4741f7017c483c958f5
ele94 <lena.ageitos@gmail.com>
2021-04-12 11:11:26 +0200

    Testing the decision window sizes

--- experiment_settings.py
+++ experiment_settings.py
@@ -8,7 +8,7 @@ def get_experiment_settings():
     new_params = {}
 
     new_params["feats_window_size"] = [10]
-    new_params["eval_window_size"] = [1]
+    new_params["eval_window_size"] = [1, 3, 5, 10]
     new_params["classifier"] = ["svm"]
     new_params["strategy"] = ["weights"]
     new_params["train_pos_range_max"] = [100]
@@ -18,15 +18,15 @@ def get_experiment_settings():
     new_params["weights_type"] = ["all"]
     new_params["weights_window_size"] = [100]
 
-    new_params["feats"] = ["combined", "tfidf", "text"]
+    new_params["feats"] = ["combined"]
     new_params["text_features"] = ["all", "select"]
     new_params["prons"] = [True]
     new_params["nssi"] = [True]
     new_params["tfidf_type"] = ["positives"]
     new_params["tfidf_ngrams"] = [False]
 
-    new_params["discretize"] = [False, True]
-    new_params["discretize_size"] = [50, 75, 100]
+    new_params["discretize"] = [True]
+    new_params["discretize_size"] = [50, 75]
     new_params["discretize_strategy"] = ['quantile']  # uniform, quantile, kmeans
     new_params["discretize_encode"] = ['onehot']   # onehot, onehot-dense, ordinal
 
